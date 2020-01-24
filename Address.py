ba16b8fa9cfdcdb00b3140fa6eeb012c99cd3a1c
Robert Mulligan <robertpmulligan@gmail.com>
2020-01-24 16:22:41 -0600

    Setting up files from the UML

--- Address.py
+++ Address.py
@@ -1,15 +1,28 @@
 class Address:
-    address: int
+    """
+    Holds a memory address
+    """
+    address: int    # Entire address
+    tag: int        # Address tag
+    index: int      # Address index
+    off_set: int      # Offset
 
-    def __init__(self, address: int):
+    def __init__(self, address: int, num_mask_index: int, num_mask_offset: int) -> None:
+        """
+        :param address: Full address
+        :param num_mask_index: Number of bits used for index
+        :param num_mask_offset: Number of bits used for offset
+        """
         self.address = address
+        self.off_set = address & ((2**num_mask_offset) - 1)             # get lower num_mask_offset number of bits
+        self.index = (address >> num_mask_offset) % (2**num_mask_index)  # Get middle bits needed for index
+        self.tag = address >> (num_mask_offset + num_mask_index)
 
-    def getTag(self) -> int:
-        return self.address  # placeholder
+    def get_tag(self) -> int:
+        return self.tag
 
-    def getIndex(self) -> int:
-        return self.address  # placeholder
+    def get_index(self) -> int:
+        return self.index
 
-
-    def getOffset(self) -> int:
-        return self.address  # placeholder
+    def get_offset(self) -> int:
+        return self.off_set
--- Cpu.py
+++ Cpu.py
@@ -3,18 +3,14 @@ from Address import Address
 
 class Cpu:
 
-
     def loadDouble(self, address: Address):
         pass
 
-
     def storeDouble(self, address: Address, value: float):
         pass
-
 
     def addDouble(self, value1: float, value2: float) -> float:
         pass
 
     def multDouble(self, value1: float, value2: float) -> float:
         pass
-
