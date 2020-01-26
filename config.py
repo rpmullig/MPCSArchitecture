3f648d1f4aaef8fcc62e4a4d3ed52e045fae50d2
Bobby Mulligan <robertpmulligan@gmail.com>
2020-01-26 13:43:26 -0600

    Still working on the configuration file. Need to know how to calculate the variables from the input and results. Still needing to read the book, but making some progress finally

--- config.py
+++ config.py
@@ -11,14 +11,23 @@ matrix_dimension: int  # x by x matrix
 print_mode: bool  # enables printing of the "solution" matrix product or daxpy vector after emulation
 blocking_factor: int  # Use in blocked matrix multiplication algorithm
 
-# computed variables
+# computed variables from configuration inputs
 ram_size: int  # computed as the minimum memory needed to hold the daxpy or matrix-matrix multiply data structures
+set_number: int  # computed
+cached_blocks: int  # computed
+
+# result variables used throughout the program
+instruction_count: int
+read_hits: int
+read_misses: int
+write_hits: int
+write_misses: int
+
+#  computed variables from emulation results
 
 
-# result variables
 
-
-def compute_variables() -> None:
+def compute_config_variables() -> None:
     pass
 
 
@@ -26,19 +35,30 @@ def print_configuration() -> None:
     """
     :return: prints the current configuration of the global variable inputs
     """
-    compute_variables()  # compute variables from the ones that were passed
+    compute_config_variables()  # compute variables from the ones that were passed
 
     print("INPUTS====================================")
     # print("Ram Size = {:26d} bytes".format(ram_size))  # computed value
-    print("Cache Size = {:26d} bytes".format(cache_size))
-    print("Block Size = {:26d} bytes".format(cache_size))
-    print("Total Blocks in cache = {:26d} bytes".format(cache_size))
-    print("Associativity = {:26d} bytes".format(cache_size))
-    print("Number of Sets: {:26d} bytes".format(cache_size))
-    print("Replacement Policy: {:26d} bytes".format(policy))
-    print("Number of Sets: {:26d} bytes".format(cache_size))
-    print("Number of Sets: {:26d} bytes".format(cache_size))
+    print("Cache Size = {:23d} bytes".format(cache_size))
+    print("Block Size = {:23d} bytes".format(block_size))
+    # print("Total Blocks in cache = {:26d} bytes".format(cached_blocks))
+    print("Associativity = {:26d}".format(n_way))
+    print("Number of Sets = {:25d}".format(cache_size))
+    print("Replacement Policy  = {:>20s}".format(policy))
+    print("Algorithm  = {:>29s}".format(algorithm))
+    print("MXM Blocking Factor  = {:19d}".format(blocking_factor))
+    print("Matrix or Vector dimension  = {:12d}".format(matrix_dimension))
+
+
+def compute_result_variables():
+    pass
 
 
 def print_results() -> None:
+    """
+    :return: prints the results of the emulation
+    """
+    compute_result_variables()
+
+    print("RESULTS====================================")
     pass
