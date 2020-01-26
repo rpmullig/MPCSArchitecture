e4a47378fac69e13e26ccbd9b1c17661c1110cbd
Bobby Mulligan <robertpmulligan@gmail.com>
2020-01-26 13:21:33 -0600

    Working on configuration of the emulator. Close to storing input values and putting them into a global scope

--- config.py
+++ config.py
@@ -11,21 +11,34 @@ matrix_dimension: int  # x by x matrix
 print_mode: bool  # enables printing of the "solution" matrix product or daxpy vector after emulation
 blocking_factor: int  # Use in blocked matrix multiplication algorithm
 
+# computed variables
+ram_size: int  # computed as the minimum memory needed to hold the daxpy or matrix-matrix multiply data structures
+
+
+# result variables
+
+
+def compute_variables() -> None:
+    pass
+
+
 def print_configuration() -> None:
     """
-    :return: prints  
+    :return: prints the current configuration of the global variable inputs
     """
+    compute_variables()  # compute variables from the ones that were passed
+
     print("INPUTS====================================")
-    print(f"Ram Size: {cache_size} bytes")
-    print(f"Cache Size: {cache_size} bytes")
-    print(f"Block Size: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    print(f"Cache: {cache_size}")
-    
-    
+    # print("Ram Size = {:26d} bytes".format(ram_size))  # computed value
+    print("Cache Size = {:26d} bytes".format(cache_size))
+    print("Block Size = {:26d} bytes".format(cache_size))
+    print("Total Blocks in cache = {:26d} bytes".format(cache_size))
+    print("Associativity = {:26d} bytes".format(cache_size))
+    print("Number of Sets: {:26d} bytes".format(cache_size))
+    print("Replacement Policy: {:26d} bytes".format(policy))
+    print("Number of Sets: {:26d} bytes".format(cache_size))
+    print("Number of Sets: {:26d} bytes".format(cache_size))
+
+
+def print_results() -> None:
+    pass
